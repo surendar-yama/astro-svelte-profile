472180e8d25970cfcb8fcd2b075df7514a3ebafc
Surendar <bug0017@outlook.com>
2022-02-22 20:13:20 +0530

    added tailwindcss

---
+++ src/layouts/base.layout.svelte
@@ -0,0 +1,18 @@
+
+<script>
+	export let title;
+</script>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width" />
+		<title>{title}</title>
+		<style global>
+			@import "src/styles/global.css";
+		</style>
+	</head>
+	<body>
+		<slot/>
+	</body>
+</html>
